198e1d7532ba9db05b71ec4a8c3162a7bcfc26cd
abdullah956 <abdullaharshed956@gmail.com>
2024-08-02 21:04:42 +0500

    .

--- Linked List/E-ReverseLinkedList.py
+++ Linked List/E-ReverseLinkedList.py
@@ -1,10 +1,9 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 class Solution:
-    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    def reverseList(self, head: ListNode) :
         prev = None
         current = head
         while current:
